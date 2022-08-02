9536655eb79ee982e46ead6c381b9b5e805f0768
Kristina <cvshibinskaya@mail.ru>
2022-08-02 18:19:46 +0300

    Решение задача с корректировкой второй задачи

--- Seminar_2/Program.cs
+++ Seminar_2/Program.cs
@@ -6,24 +6,26 @@
 // Console.Write(number_2);
 
 // // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-// Console.Write("Input a number: ");
-// int number = Convert.ToInt32(Console.ReadLine());
-// int number_1 = number/100;
-// if (number_1 == 0)
-// Console.Write("no third digit");
-// else
-// Console.Write(number % 10);
-
-// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
-Console.Write("Enter a number representing the day of the week: ");
+Console.Write("Input a number: ");
 int number = Convert.ToInt32(Console.ReadLine());
-if (number == 6)
-Console.Write("Day off");
-else
-if (number == 7)
-Console.Write("Day off");
-else
-if (number > 0 & number < 6)
-Console.Write("Working day");
-else
-Console.Write("Please enter a valid number");
+while (number >= 1000)
+number = number / 10;
+if (number >= 100 & number < 1000)
+Console.Write(number % 10);
+else if (number < 100)
+Console.Write("no third digit");
+
+
+// // Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// Console.Write("Enter a number representing the day of the week: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if (number == 6)
+// Console.Write("Day off");
+// else
+// if (number == 7)
+// Console.Write("Day off");
+// else
+// if (number > 0 & number < 6)
+// Console.Write("Working day");
+// else
+// Console.Write("Please enter a valid number");
